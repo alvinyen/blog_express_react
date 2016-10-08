357c06fcd0e087c5602c04e3fa31a4bcf8d07545
yamapi06150 <yamapi06150@gmail.com>
2016-10-08 21:28:54 +0800

    first commit - testing express-react-view

---
+++ README.md
@@ -0,0 +1,2 @@
+npm install babel-preset-es2015 --save --no-bin-links
+npm install babel-preset-react --save --no-bin-links
---
+++ views/error.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+class ErrorMessage extends React.Component{
+    render(){
+        return <div><h1> Error Messgae ！！</h1></div> ;
+    }
+}
+
+module.exports = ErrorMessage ;
