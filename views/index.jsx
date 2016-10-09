163ec97b1726ab4f477413d152f71a94332a276e
yamapi06150 <yamapi06150@gmail.com>
2016-10-09 23:43:59 +0800

    second commit - adding a react component with props

--- views/index.jsx
+++ views/index.jsx
@@ -1,9 +1,21 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 
-class HelloMessage extends React.Component{
+class HelloMessage extends React.Component {
     render(){
-        return <div> Hello {this.props.name}</div> ;
+        return <div>
+                    <h1>YOYOIOIO</h1><br/>
+                    <div> Hello {this.props.age}</div>
+                 </div> ;
     }
 }
+
+// var HelloMessage = React.createClass({
+//     render: function(){
+//         return <div>Hello {this.props.name}</div>;
+//     }
+// });
+
+// ReactDOM.render(<HelloMessage/>,document.getElementsBy..);
 
 module.exports = HelloMessage ;
